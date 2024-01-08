c9852103ff3558b5adc686579e8be23e53d19a31
Jhoberth Bravo <>
2024-01-08 15:50:53 -0400

    Change app react

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BookDetails from './components/BookDetails/BookDetails';
-import BookList from './components/BookList/BookList';
-import { AppProvider } from './context.';
 import './index.css';
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
-import HomePage from './pages/HomePage/HomePage';
-import Library from './pages/Library/Library';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<AppProvider>
-		<BrowserRouter>
-			<Routes>
-				<Route path='dashboard' element={<Home />}>
-					<Route path='about' element={<About />} />
-					<Route path='book' element={<BookList />} />
-				</Route>
-				<Route path='/' element={<HomePage />} />
-				<Route path='register' element={<Register />} />
-				<Route path='login' element={<Login />} />
-				<Route path='library' element={<Library />} />
-				<Route path='book/:id' element={<BookDetails />} />
-			</Routes>
-		</BrowserRouter>
-	</AppProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
